8c2c96ea7e924dfcf7f6b584677c9bcb005bd5d1
Salah Ezzat <salahezzat059@gmail.com>
2023-04-10 04:53:11 +0200

    phplab4

--- data.php
+++ data.php
@@ -1,6 +1,22 @@
+
+<html>
+  <head>
+    <style>
+        .button {
+            padding: 8px;
+            background-color: green;
+            color: white;
+            font-size:15px;
+            list-style-type: none;
+            margin-left:200px;
+            margin-bottom:20px;
+        }
+    </style>
+    </head>
+    </html>
 <?php
 echo "<h3> Registration data </h3>";
-echo "<a href='./index.html'> <button> Add new user</button></a>";
+echo "<a   href='./index.html'> <button class ='button'> Add new user</button></a>";
 $name = $_POST['name'];
 $email = $_POST['email'];
 $gender = $_POST['gender'];
@@ -18,35 +34,46 @@ if(! $conn ) {
 //    mail_statuss  Boolean NOT NULL,
 //    primary key ( id ))";
 // mysqli_query($conn ,$sql);
-$sql = "INSERT INTO users (namee, email, gender , mail_statuss) VALUES ('$name', '$email', '$gender', '$check')";
-mysqli_query($conn, $sql);
+$sql = "INSERT INTO users (Namee, Email, gender , mail_statuss) VALUES ('$name', '$email', '$gender', '$check')";
+$retval=mysqli_query($conn, $sql);
 
+  
+?>
+<table border="1">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Email</th>
+      <th>Gender</th>
+      <th>mail_statuss</th>
+    </tr>
+  </thead>
 
-
-if( isset($_POST['submit'] )){
-  $sql = "SELECT * FROM users";
-$result = mysqli_query($conn, $sql);
-
-echo "<table border='1'>";
-echo "<tr>
-<th>ID</th>
-<th>Namee</th>
-<th>Email</th>
-<th>Gender</th>
-<th>Mail_status</th>
-</tr>";
-while ($row = mysqli_fetch_assoc($result)) {
-    echo "<tr>
-    <td>". "{$row['id'] }"."</td>
-    <td>". "{$row['Namee'] }"."</td> 
-    <td>". "{$row['Email']}" ."</td>
-    <td>"."{$row['gender']}"."</td> ".
-    "<td>"."{$row['mail_statuss'] }"."</td>
-    </tr>";
+<tbody>
+  <?php
+  $sql1 = "SELECT id ,Namee , Email , gender, mail_statuss FROM users";
+$result = mysqli_query($conn, $sql1);
+if (mysqli_num_rows($result) > 0){
+  foreach($result as $row) {
+    ?>
+    <tr>
+      <td> <?=$row['id']?></td>
+      <td> <?=$row['Namee']?></td>
+      <td> <?=$row['Email']?></td>
+      <td> <?=$row['gender']?></td>
+      <td> <?=$row['mail_statuss']?></td>
+    </tr>
+    
+    <?php
+  }
+  ?>
+</tbody>
+</table>
+<?php
 }
-
-echo "</table>";
-
+else {
+  echo "0 results";
+}
 mysqli_close($conn);
-}
 ?>
